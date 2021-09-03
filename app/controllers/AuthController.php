7148959e1365af06208a6de81c8eb064e89f4b53
Evgeni Kolev <ekoleww@gmail.com>
2021-09-03 10:44:13 +0300

    added login and register logic

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -5,22 +5,97 @@ declare(strict_types=1);
 namespace Controllers;
 
 use Core\Controller;
+use Core\Exceptions\EmptyFieldException;
+use Core\Exceptions\UserNotFoundException;
+use Core\Exceptions\InvalidArgumentException;
+use Core\Exceptions\UsernameAlreadyTakenException;
 use Core\Request;
+use Models\User;
+use Utility\Auth;
 
 class AuthController extends Controller
 {
+    private User $userModel;
 
-    public function login() : mixed
+    private array $params = [
+        'username' => '',
+        'password' => '',
+        'firstname' => '',
+        'lastname' => ''
+    ];
+
+    private array $data = [
+        'error' => ''
+    ];
+
+    public function __construct()
+    {
+        $this->userModel = new User();
+    }
+
+    public function login(Request $request) : mixed
     {   
+        if (Auth::isLoggedIn()) {
+            return $this->render('homepage');
+        }
+
+        if ($request->isPost()) {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $this->params = [
+                'username' => $_POST['username'],
+                'password' => $_POST['password']
+            ];
+
+            try {
+                $user = [];
+                $user = $this->userModel->findByUsername($this->params['username']);
+                
+                if (!Auth::authenticate($this->params, $user)) {
+                    return $this->setData('login', 'Invalid username or password');
+                }
+            
+            Auth::setUser($user);
+            return $this->render('homepage');
+
+            } catch(InvalidArgumentException $exception) {
+                return $this->setData('login',$exception->getMessage() );
+            } catch(UserNotFoundException $exception) {
+                return $this->setData('login',$exception->getMessage() );
+            }
+        }
+
         return $this->render('login');
     }
 
     public function register(Request $request) : mixed
     {
         if ($request->isPost()) {
-            return 'Handle submitted data';
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $this->params = [
+                'username' => $_POST['username'],
+                'password' => $_POST['password'],
+                'firstname' => $_POST['firstname'],
+                'lastname' => $_POST['lastname']
+            ];
+
+            try{
+                $this->userModel->save($this->params);
+                return $this->render('login');
+                
+            }catch(EmptyFieldException $exception) {
+                return $this->setData('register', $exception->getMessage());
+            }catch(UsernameAlreadyTakenException $exception) {
+                return $this->setData('register', $exception->getMessage());
+            }
         }
         
         return $this->render('register');
     }
+
+    private function setData($view, $error)
+    {   
+        $this->data['error'] = $error;
+        return $this->render($view, $this->data);
+    }
+
 }
