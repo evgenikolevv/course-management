628ced16a240fe7f31c05e284961edf4ab229e1c
Evgeni Kolev <ekoleww@gmail.com>
2021-09-05 10:10:54 +0300

    added course's endpoints

--- public/index.php
+++ public/index.php
@@ -4,14 +4,23 @@ require_once '../app/config/config.ini.php';
 
 use Controllers\AuthController;
 use Controllers\SiteController;
+use Controllers\CourseController;
 use Core\Application;
 
 $app = new Application(APP_ROOT, VIEW_ROOT);
 
 $app->router->get('/', [SiteController::class, 'loadHomePage']);
+$app->router->get('/homepage', [SiteController::class, 'loadHomePage']);
+
 $app->router->get('/login', [AuthController::class, 'login']);
 $app->router->post('/login', [AuthController::class, 'login']);
 $app->router->get('/register', [AuthController::class, 'register']);
 $app->router->post('/register', [AuthController::class, 'register']);
+$app->router->get('/logout', [AuthController::class, 'logout']);
+
+$app->router->get('/courses', [CourseController::class, 'findAll']);
+$app->router->get('/mycourses', [CourseController::class, 'findAllByUserId']);
+$app->router->post('/courses', [CourseController::class, 'addCourseToFavourite']);
+$app->router->post('/mycourses',[CourseController::class, 'removeCourseFromFavourite']);
 
 $app->run();
