6e2e26f4bf19bbd2709de210dc5dbab14cb21c19
Evgeni Kolev <ekoleww@gmail.com>
2021-09-05 10:13:52 +0300

    only return homepage

--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 namespace Controllers;
 
 use Core\Controller;
-use Core\Request;
 
 class SiteController extends Controller
-{
-
+{   
     public function loadHomePage() : mixed
     {   
         return $this->render('homepage');
