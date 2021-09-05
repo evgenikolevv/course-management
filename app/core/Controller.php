c06c43eebd525af54270c95e2fa3ebe1b6653450
Evgeni Kolev <ekoleww@gmail.com>
2021-09-05 10:11:53 +0300

    added check for authorization in render

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace Core;
 
 use Core\Application;
+use Utility\Auth;
 
 class Controller
 {   
-    
     protected function render(string $view, array $data = []) : mixed
     {
+        if ($view != 'login' && $view != 'register' && !Auth::isLoggedIn()) {
+            return $this->render('login');
+        }
         return Application::$app->router->renderView($view,$data);
     }
 }
